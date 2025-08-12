8b9d5208bac5297a5ffbfab1c52cfba8f989d186
TheFateless <52730833+TheFateless@users.noreply.github.com>
2025-08-12 15:07:19 -0600

    "just anothe string"

---
+++ variables.py
@@ -0,0 +1,23 @@
+# set up our variables
+num1 = 3.1415926535897932846264338
+num2 = 2
+num3 = 16.00
+
+# create expressions and store results
+add_result = num1 + num2
+subrtact_check = num2 - num1
+divide_check = num3 / num2
+exponent_result = num3 ** num2
+remainder_result = num3 % num2
+greater_than_check = num1 > num2
+multiple_ops_check = round((divide_check * 5) ** num1, 2)
+
+# print results
+
+print(add_result)
+print(subrtact_check)
+print(divide_check)
+print(exponent_result)
+print(remainder_result)
+print(greater_than_check)
+print(multiple_ops_check)
